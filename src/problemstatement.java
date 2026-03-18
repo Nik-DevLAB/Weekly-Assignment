5529039e02e1f4fcc602206a6d3c91cc6967af1b
Nik-DevLAB <na8647@srmist.edu.in>
2026-03-18 16:01:10 +0530

    problem statement - 6

--- src/problemstatement.java
+++ src/problemstatement.java
@@ -1,27 +1,36 @@
 import java.util.*;
 
+class Bucket {
+    int tokens = 5;
+    long last = System.currentTimeMillis();
+
+    boolean allow() {
+        long now = System.currentTimeMillis();
+        if (now - last > 1000) {
+            tokens = 5;
+            last = now;
+        }
+
+        if (tokens > 0) {
+            tokens--;
+            return true;
+        }
+        return false;
+    }
+}
+
 public class problemstatement {
 
-    static Map<String, Integer> views = new HashMap<>();
-    static Map<String, Set<String>> users = new HashMap<>();
-    static Map<String, Integer> sources = new HashMap<>();
+    static Map<String, Bucket> map = new HashMap<>();
 
-    static void process(String url, String user, String source) {
-        views.put(url, views.getOrDefault(url, 0) + 1);
-
-        users.putIfAbsent(url, new HashSet<>());
-        users.get(url).add(user);
-
-        sources.put(source, sources.getOrDefault(source, 0) + 1);
+    static boolean check(String client) {
+        map.putIfAbsent(client, new Bucket());
+        return map.get(client).allow();
     }
 
     public static void main(String[] args) {
-        process("/news", "u1", "google");
-        process("/news", "u2", "facebook");
-        process("/sports", "u1", "google");
-
-        System.out.println("Views: " + views);
-        System.out.println("Unique Users: " + users);
-        System.out.println("Sources: " + sources);
+        for (int i = 0; i < 7; i++) {
+            System.out.println(check("user1"));
+        }
     }
 }
