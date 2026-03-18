888b5999083d41b0133624fac32cffce0990e725
Nik-DevLAB <na8647@srmist.edu.in>
2026-03-18 16:10:49 +0530

    problem statement - 10

--- src/problemstatement.java
+++ src/problemstatement.java
@@ -1,23 +1,39 @@
 import java.util.*;
 
+class LRU extends LinkedHashMap<String, String> {
+    int cap;
+
+    LRU(int cap) {
+        super(cap, 0.75f, true);
+        this.cap = cap;
+    }
+
+    protected boolean removeEldestEntry(Map.Entry<String, String> e) {
+        return size() > cap;
+    }
+}
+
 public class problemstatement {
 
-    static void find(int[] arr, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
+    static LRU L1 = new LRU(2);
+    static LRU L2 = new LRU(5);
 
-        for (int num : arr) {
-            int comp = target - num;
+    static String get(String key) {
+        if (L1.containsKey(key)) return "L1 HIT";
 
-            if (map.containsKey(comp)) {
-                System.out.println(num + " + " + comp);
-            }
-
-            map.put(num, 1);
+        if (L2.containsKey(key)) {
+            L1.put(key, L2.get(key));
+            return "L2 HIT → promoted";
         }
+
+        L2.put(key, "DATA");
+        return "DB HIT";
     }
 
     public static void main(String[] args) {
-        int[] arr = {500, 300, 200};
-        find(arr, 500);
+        System.out.println(get("A"));
+        System.out.println(get("A"));
+        System.out.println(get("B"));
+        System.out.println(get("C"));
     }
 }
